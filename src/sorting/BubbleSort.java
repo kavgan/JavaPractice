4d6e91e8d80bebfa200c244e4811a5fb5e2687b1
kganes2 <ganesan.kavita@gmail.com>
2016-01-29 14:56:38 -0700

    updated comments

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,7 +1,7 @@
 package sorting;
 
 /**
- * @author Kavita Ganesan 
+ * @author Kavita Ganesan  O(N^2)
  * http://www.kavita-ganesan.com
  */
 public class BubbleSort {
@@ -17,7 +17,7 @@ public class BubbleSort {
 			//default to already sorted
 			sorted=true;
 			
-			//for each i in array check i+1
+			//for each i in array check i+1, bubble up the values
 			for(int i=0; i< intArray.length-1; i++){
 				if(intArray[i]>intArray[i+1]){
 					sorted=false; //make it not sorted
