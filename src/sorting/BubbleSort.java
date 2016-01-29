c5fa561b6abfae966bfb37290a17e90793ee3683
kganes2 <ganesan.kavita@gmail.com>
2016-01-29 13:34:57 -0700

    BubbleSort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int [] intArray={1,4,2,6,5,3,2};
+
+		//for each i in array 
+		for(int i=0; i< intArray.length; i++){
+			
+			//check if intArray[i] is bigger than any of the elements
+			//to the right of i. If it is, swap. Now use the swapped 
+			//value and figure out if its bigger than the remaining values
+			// not compared against. Repeat.
+			for(int m=i; m< intArray.length; m++){
+				if(m!=i && intArray[i]>intArray[m]){
+					int temp=intArray[m];
+					intArray[m]=intArray[i];
+					intArray[i]=temp;
+				}
+			}
+		}
+
+		System.out.println("Sorted array:");
+		for(int i:intArray){
+			System.out.println(i);
+		}
+	}
+}
